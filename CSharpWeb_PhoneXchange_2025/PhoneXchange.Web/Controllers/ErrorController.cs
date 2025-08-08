e90d1ec6dd9dde7de9672e0505afd8a3e8061c36
Atanas Uluchev <uluchev@abv.bg>
2025-08-09 00:30:56 +0300

    new controller

---
+++ CSharpWeb_PhoneXchange_2025/PhoneXchange.Web/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneXchange.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/404")]
+        public IActionResult Error404()
+        {
+            Response.StatusCode = 404;
+            return View("Error404");
+        }
+
+        [Route("Error/500")]
+        public IActionResult Error500()
+        {
+            Response.StatusCode = 500;
+            return View("Error500");
+        }
+    }
+}
